Suwayomi/Suwayomi-Server

commit 1c9a139006f7a9e399c964b2a88650fb757d8369

Always return "ArchiveProvider" in case "downloadAsCbz" is enabled (#671)
* Move chapter download logic to base class

* Do not reuse "FolderProvider" in "ArchiveProviders" download function

Due to reusing the "FolderProvider" to download a chapter as a cbz file, a normal chapter download folder was created.
In case the download was aborted before the cbz file got created and the folder deleted, the next time the chapter got downloaded, the wrong "FileProvider" was selected, causing the chapter not to get downloaded as a cbz file.

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/ChaptersFilesProvider.kt
@@ -1,7 +1,16 @@
 package suwayomi.tachidesk.manga.impl.download.fileProvider
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.sample
+import suwayomi.tachidesk.manga.impl.Page
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
+import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
+import java.io.File
 import java.io.InputStream
 
 /*
@@ -14,11 +23,46 @@ abstract class ChaptersFilesProvider(val mangaId: Int, val chapterId: Int) : Dow
         return RetrieveFile1Args(::getImageImpl)
     }
 
-    abstract suspend fun downloadImpl(
+    @OptIn(FlowPreview::class)
+    open suspend fun downloadImpl(
         download: DownloadChapter,
         scope: CoroutineScope,
         step: suspend (DownloadChapter?, Boolean) -> Unit
-    ): Boolean
+    ): Boolean {
+        val pageCount = download.chapter.pageCount
+        val chapterDir = getChapterCachePath(mangaId, chapterId)
+        val folder = File(chapterDir)
+        folder.mkdirs()
+
+        for (pageNum in 0 until pageCount) {
+            var pageProgressJob: Job? = null
+            val fileName = Page.getPageName(pageNum) // might have to change this to index stored in database
+            if (File(folder, fileName).exists()) continue
+            try {
+                Page.getPageImage(
+                    mangaId = download.mangaId,
+                    chapterIndex = download.chapterIndex,
+                    index = pageNum
+                ) { flow ->
+                    pageProgressJob = flow
+                        .sample(100)
+                        .distinctUntilChanged()
+                        .onEach {
+                            download.progress = (pageNum.toFloat() + (it.toFloat() * 0.01f)) / pageCount
+                            step(null, false) // don't throw on canceled download here since we can't do anything
+                        }
+                        .launchIn(scope)
+                }
+            } finally {
+                // always cancel the page progress job even if it throws an exception to avoid memory leaks
+                pageProgressJob?.cancel()
+            }
+            // TODO: retry on error with 2,4,8 seconds of wait
+            download.progress = ((pageNum + 1).toFloat()) / pageCount
+            step(download, false)
+        }
+        return true
+    }
 
     override fun download(): FileDownload3Args<DownloadChapter, CoroutineScope, suspend (DownloadChapter?, Boolean) -> Unit> {
         return FileDownload3Args(::downloadImpl)

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/ArchiveProvider.kt
@@ -9,8 +9,8 @@ import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream
 import org.apache.commons.compress.archivers.zip.ZipFile
 import suwayomi.tachidesk.manga.impl.download.fileProvider.ChaptersFilesProvider
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
+import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
 import suwayomi.tachidesk.manga.impl.util.getChapterCbzPath
-import suwayomi.tachidesk.manga.impl.util.getChapterDownloadPath
 import java.io.File
 import java.io.InputStream
 
@@ -29,20 +29,19 @@ class ArchiveProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(mang
         scope: CoroutineScope,
         step: suspend (DownloadChapter?, Boolean) -> Unit
     ): Boolean {
-        val chapterDir = getChapterDownloadPath(mangaId, chapterId)
         val outputFile = File(getChapterCbzPath(mangaId, chapterId))
-        val chapterFolder = File(chapterDir)
-        if (outputFile.exists()) handleExistingCbzFile(outputFile, chapterFolder)
+        val chapterCacheFolder = File(getChapterCachePath(mangaId, chapterId))
+        if (outputFile.exists()) handleExistingCbzFile(outputFile, chapterCacheFolder)
 
-        FolderProvider(mangaId, chapterId).download().execute(download, scope, step)
+        super.downloadImpl(download, scope, step)
 
         withContext(Dispatchers.IO) {
             outputFile.createNewFile()
         }
 
         ZipArchiveOutputStream(outputFile.outputStream()).use { zipOut ->
-            if (chapterFolder.isDirectory) {
-                chapterFolder.listFiles()?.sortedBy { it.name }?.forEach {
+            if (chapterCacheFolder.isDirectory) {
+                chapterCacheFolder.listFiles()?.sortedBy { it.name }?.forEach {
                     val entry = ZipArchiveEntry(it.name)
                     try {
                         zipOut.putArchiveEntry(entry)
@@ -56,8 +55,8 @@ class ArchiveProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(mang
             }
         }
 
-        if (chapterFolder.exists() && chapterFolder.isDirectory) {
-            chapterFolder.deleteRecursively()
+        if (chapterCacheFolder.exists() && chapterCacheFolder.isDirectory) {
+            chapterCacheFolder.deleteRecursively()
         }
 
         return true

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/FolderProvider.kt
@@ -1,18 +1,10 @@
 package suwayomi.tachidesk.manga.impl.download.fileProvider.impl
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.sample
-import suwayomi.tachidesk.manga.impl.Page
-import suwayomi.tachidesk.manga.impl.Page.getPageName
 import suwayomi.tachidesk.manga.impl.download.fileProvider.ChaptersFilesProvider
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
+import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
 import suwayomi.tachidesk.manga.impl.util.getChapterDownloadPath
-import suwayomi.tachidesk.manga.impl.util.storage.ImageResponse
 import java.io.File
 import java.io.FileInputStream
 import java.io.InputStream
@@ -30,47 +22,28 @@ class FolderProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(manga
         return Pair(FileInputStream(file).buffered(), "image/$fileType")
     }
 
-    @OptIn(FlowPreview::class)
     override suspend fun downloadImpl(
         download: DownloadChapter,
         scope: CoroutineScope,
         step: suspend (DownloadChapter?, Boolean) -> Unit
     ): Boolean {
-        val pageCount = download.chapter.pageCount
         val chapterDir = getChapterDownloadPath(mangaId, chapterId)
         val folder = File(chapterDir)
-        folder.mkdirs()
 
-        for (pageNum in 0 until pageCount) {
-            var pageProgressJob: Job? = null
-            val fileName = getPageName(pageNum) // might have to change this to index stored in database
-            if (isExistingFile(folder, fileName)) continue
-            try {
-                Page.getPageImage(
-                    mangaId = download.mangaId,
-                    chapterIndex = download.chapterIndex,
-                    index = pageNum
-                ) { flow ->
-                    pageProgressJob = flow
-                        .sample(100)
-                        .distinctUntilChanged()
-                        .onEach {
-                            download.progress = (pageNum.toFloat() + (it.toFloat() * 0.01f)) / pageCount
-                            step(null, false) // don't throw on canceled download here since we can't do anything
-                        }
-                        .launchIn(scope)
-                }.first.use { image ->
-                    val filePath = "$chapterDir/$fileName"
-                    ImageResponse.saveImage(filePath, image)
-                }
-            } finally {
-                // always cancel the page progress job even if it throws an exception to avoid memory leaks
-                pageProgressJob?.cancel()
-            }
-            // TODO: retry on error with 2,4,8 seconds of wait
-            download.progress = ((pageNum + 1).toFloat()) / pageCount
-            step(download, false)
+        val alreadyDownloaded = folder.exists()
+        if (alreadyDownloaded) {
+            return true
         }
+
+        val downloadSucceeded = super.downloadImpl(download, scope, step)
+        if (!downloadSucceeded) {
+            return false
+        }
+
+        folder.mkdirs()
+        val cacheChapterDir = getChapterCachePath(mangaId, chapterId)
+        File(cacheChapterDir).renameTo(folder)
+
         return true
     }
 

MMajor87/Suwayomi-Server-Multi-User

commit a59e4af8c79fcffc2726aa6d0823b861f5f602be

Fix/recognition of already downloaded chapters (#922)
* Remove overrides of "ChapterFilesProvider::downloadImpl"

* Check final download folder for existing page on download

Downloads were changed to get downloaded to the system temp folder instead to directly into the final download folder.

This broke the check for existing pages, because now only the temp folder was checked instead of both the temp and the final download folder.

Regression introduced with 1c9a139006f7a9e399c964b2a88650fb757d8369

* Properly check for already existing downloaded pages

The previous check was always false because the file ending of the page file is unknown and thus, missing from the created file path

* Cleanup cache download folder

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/ChaptersFilesProvider.kt
@@ -13,6 +13,8 @@ import suwayomi.tachidesk.manga.impl.Page
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
 import suwayomi.tachidesk.manga.impl.util.createComicInfoFile
 import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
+import suwayomi.tachidesk.manga.impl.util.getChapterDownloadPath
+import suwayomi.tachidesk.manga.impl.util.storage.ImageResponse
 import suwayomi.tachidesk.manga.model.table.ChapterTable
 import suwayomi.tachidesk.manga.model.table.MangaTable
 import java.io.File
@@ -28,21 +30,40 @@ abstract class ChaptersFilesProvider(val mangaId: Int, val chapterId: Int) : Dow
         return RetrieveFile1Args(::getImageImpl)
     }
 
+    /**
+     * Extract the existing download to the base download folder (see [getChapterDownloadPath])
+     */
+    protected abstract fun extractExistingDownload()
+
+    protected abstract suspend fun handleSuccessfulDownload()
+
     @OptIn(FlowPreview::class)
-    open suspend fun downloadImpl(
+    private suspend fun downloadImpl(
         download: DownloadChapter,
         scope: CoroutineScope,
         step: suspend (DownloadChapter?, Boolean) -> Unit,
     ): Boolean {
-        val pageCount = download.chapter.pageCount
-        val chapterDir = getChapterCachePath(mangaId, chapterId)
-        val folder = File(chapterDir)
-        folder.mkdirs()
+        extractExistingDownload()
 
+        val finalDownloadFolder = getChapterDownloadPath(mangaId, chapterId)
+
+        val cacheChapterDir = getChapterCachePath(mangaId, chapterId)
+        val downloadCacheFolder = File(cacheChapterDir)
+        downloadCacheFolder.mkdirs()
+
+        val pageCount = download.chapter.pageCount
         for (pageNum in 0 until pageCount) {
             var pageProgressJob: Job? = null
             val fileName = Page.getPageName(pageNum) // might have to change this to index stored in database
-            if (File(folder, fileName).exists()) continue
+
+            val pageExistsInFinalDownloadFolder = ImageResponse.findFileNameStartingWith(finalDownloadFolder, fileName) != null
+            val pageExistsInCacheDownloadFolder = ImageResponse.findFileNameStartingWith(cacheChapterDir, fileName) != null
+
+            val doesPageAlreadyExist = pageExistsInFinalDownloadFolder || pageExistsInCacheDownloadFolder
+            if (doesPageAlreadyExist) {
+                continue
+            }
+
             try {
                 Page.getPageImage(
                     mangaId = download.mangaId,
@@ -69,7 +90,7 @@ abstract class ChaptersFilesProvider(val mangaId: Int, val chapterId: Int) : Dow
         }
 
         createComicInfoFile(
-            folder.toPath(),
+            downloadCacheFolder.toPath(),
             transaction {
                 MangaTable.select { MangaTable.id eq mangaId }.first()
             },
@@ -77,6 +98,10 @@ abstract class ChaptersFilesProvider(val mangaId: Int, val chapterId: Int) : Dow
                 ChapterTable.select { ChapterTable.id eq chapterId }.first()
             },
         )
+
+        handleSuccessfulDownload()
+
+        File(cacheChapterDir).deleteRecursively()
 
         return true
     }

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/ArchiveProvider.kt
@@ -1,6 +1,5 @@
 package suwayomi.tachidesk.manga.impl.download.fileProvider.impl
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
@@ -11,7 +10,6 @@ import org.kodein.di.DI
 import org.kodein.di.conf.global
 import org.kodein.di.instance
 import suwayomi.tachidesk.manga.impl.download.fileProvider.ChaptersFilesProvider
-import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
 import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
 import suwayomi.tachidesk.manga.impl.util.getChapterCbzPath
 import suwayomi.tachidesk.manga.impl.util.getMangaDownloadDir
@@ -32,17 +30,21 @@ class ArchiveProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(mang
         return Pair(inputStream.buffered(), "image/$fileType")
     }
 
-    override suspend fun downloadImpl(
-        download: DownloadChapter,
-        scope: CoroutineScope,
-        step: suspend (DownloadChapter?, Boolean) -> Unit,
-    ): Boolean {
+    override fun extractExistingDownload() {
+        val outputFile = File(getChapterCbzPath(mangaId, chapterId))
+        val chapterCacheFolder = File(getChapterCachePath(mangaId, chapterId))
+
+        if (!outputFile.exists()) {
+            return
+        }
+
+        extractCbzFile(outputFile, chapterCacheFolder)
+    }
+
+    override suspend fun handleSuccessfulDownload() {
         val mangaDownloadFolder = File(getMangaDownloadDir(mangaId))
         val outputFile = File(getChapterCbzPath(mangaId, chapterId))
         val chapterCacheFolder = File(getChapterCachePath(mangaId, chapterId))
-        if (outputFile.exists()) handleExistingCbzFile(outputFile, chapterCacheFolder)
-
-        super.downloadImpl(download, scope, step)
 
         withContext(Dispatchers.IO) {
             mangaDownloadFolder.mkdirs()
@@ -68,8 +70,6 @@ class ArchiveProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(mang
         if (chapterCacheFolder.exists() && chapterCacheFolder.isDirectory) {
             chapterCacheFolder.deleteRecursively()
         }
-
-        return true
     }
 
     override fun delete(): Boolean {
@@ -83,7 +83,7 @@ class ArchiveProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(mang
         return cbzDeleted
     }
 
-    private fun handleExistingCbzFile(
+    private fun extractCbzFile(
         cbzFile: File,
         chapterFolder: File,
     ) {

server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/fileProvider/impl/FolderProvider.kt
@@ -1,11 +1,9 @@
 package suwayomi.tachidesk.manga.impl.download.fileProvider.impl
 
-import kotlinx.coroutines.CoroutineScope
 import org.kodein.di.DI
 import org.kodein.di.conf.global
 import org.kodein.di.instance
 import suwayomi.tachidesk.manga.impl.download.fileProvider.ChaptersFilesProvider
-import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
 import suwayomi.tachidesk.manga.impl.util.getChapterCachePath
 import suwayomi.tachidesk.manga.impl.util.getChapterDownloadPath
 import suwayomi.tachidesk.manga.impl.util.storage.FileDeletionHelper
@@ -29,23 +27,16 @@ class FolderProvider(mangaId: Int, chapterId: Int) : ChaptersFilesProvider(manga
         return Pair(FileInputStream(file).buffered(), "image/$fileType")
     }
 
-    override suspend fun downloadImpl(
-        download: DownloadChapter,
-        scope: CoroutineScope,
-        step: suspend (DownloadChapter?, Boolean) -> Unit,
-    ): Boolean {
+    override fun extractExistingDownload() {
+        // nothing to do
+    }
+
+    override suspend fun handleSuccessfulDownload() {
         val chapterDir = getChapterDownloadPath(mangaId, chapterId)
         val folder = File(chapterDir)
 
-        val downloadSucceeded = super.downloadImpl(download, scope, step)
-        if (!downloadSucceeded) {
-            return false
-        }
-
         val cacheChapterDir = getChapterCachePath(mangaId, chapterId)
         File(cacheChapterDir).copyRecursively(folder, true)
-
-        return true
     }
 
     override fun delete(): Boolean {